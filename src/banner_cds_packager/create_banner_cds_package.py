48fec5c7936a36d6f72b67a804f55f743436526c
Adam Franco <afranco@middlebury.edu>
2026-03-23 14:01:18 -0400

    Remove unneeded **. Can just use *

--- src/banner_cds_packager/create_banner_cds_package.py
+++ src/banner_cds_packager/create_banner_cds_package.py
@@ -16,13 +16,13 @@ def create_banner_cds_package(
     # Options for identifying changed banner files to add to the package.
 
     # SQL
-    object_create_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match table and object definition file patterns. Can be specified multiple times.")] = ["*/table/*.create.sql", "**/object/*.create.sql"],
+    object_create_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match table and object definition file patterns. Can be specified multiple times.")] = ["*/table/*.create.sql", "*/object/*.create.sql"],
     object_setup_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match table and object setup file patterns. Can be specified multiple times.")] = ["*/table/*.setup.sql", "*/object/*.setup.sql"],
     object_dml_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match table and object DML file patterns. Can be specified multiple times.")] = ["*/table/*.dml.sql", "*/object/*.dml.sql"],
     function_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match file patterns. Can be specified multiple times.")] = ["*/function/*.fnc"],
     view_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match view file patterns. Can be specified multiple times.")] = ["*/views/*.vw"],
     matview_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match materialized view file patterns. Can be specified multiple times.")] = ["*/matview/*.mv", "*/matview/*.sql", "*/matview/*.vw"],
-    sequence_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match sequence file patterns. Can be specified multiple times.")] = ["**/sequence/*.sql"],
+    sequence_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match sequence file patterns. Can be specified multiple times.")] = ["*/sequence/*.sql"],
     package_spec_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match package specification file patterns. Can be specified multiple times.")] = ["*/package/*.pks"],
     package_body_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match package body file patterns. Can be specified multiple times.")] = ["*/package/*.pkb"],
     procedure_pattern: Annotated[list[str], typer.Option(help="A glob pattern that will match procedure file patterns. Can be specified multiple times.")] = ["*/procedure/*.prc"],
