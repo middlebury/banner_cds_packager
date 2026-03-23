96e34e257e4b1c9caa4db209072eb6bcbd4885f2
Adam Franco <afranco@middlebury.edu>
2026-03-23 14:26:10 -0400

    Add command help

--- src/banner_cds_packager/cli.py
+++ src/banner_cds_packager/cli.py
@@ -3,7 +3,7 @@ import typer
 from .create_banner_cds_package import create_banner_cds_package
 
 app = typer.Typer()
-app.command()(create_banner_cds_package)
+app.command(help="Package changes to a Banner repository for deployment via Banner CDS. Must be run from within a git repository or git working directory.")(create_banner_cds_package)
 
 if __name__ == "__main__":
     app()
